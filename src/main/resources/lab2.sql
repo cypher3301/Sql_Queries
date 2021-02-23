6dfaf817b4d78d69be30688ebd2836ab39ae9416
Admin <cypher.is.3301@gmail.com>
2021-02-23 11:09:12 +0200

    Add description and fixes

--- src/main/resources/lab2.sql
+++ src/main/resources/lab2.sql
@@ -1,55 +1,76 @@
-# postgresql
--- Lab2
--- 1
--- select "CustomerId", "FirstName", "LastName", "Country" from "Customer" where "Country"!='USA';
-
--- 2
--- select * from "Customer" where "Country"='Brazil';
-
--- 3
--- select "FirstName","LastName", "InvoiceId","InvoiceDate","Country" from "Customer", "Invoice" where "Country"='Brazil';
-
--- 4
--- select * from "Employee" where "Title" like '%Sales%';
-
--- 5~
--- select DISTINCT "BillingCountry" from "Invoice";
--- select distinct C."LastName", C."FirstName"
--- from "Employee"
---          join "Customer" C
---               on "Employee"."EmployeeId" = C."SupportRepId"
---          join "Invoice" I
---               on C."CustomerId" = I."CustomerId"
--- where "Title" like '%Sales%';
--- 6
--- select "Total",
---        C."FirstName",
---        C."LastName",
---        C."Country",
---        "Title",
---        E."FirstName",
---        E."LastName"
--- from "Invoice"
---          join "Customer" C
---               on "Invoice"."CustomerId" = C."CustomerId"
---          join "Employee" E
---              on C."SupportRepId" = E."EmployeeId"
--- where "Title"  ilike ('%sale%')
-
-
-# Mysql
--- # 7
--- # select count(InvoiceDate) from invoice
---                                        # where date(InvoiceDate)>='2009/01/01' and date(InvoiceDate)<='2011/01/01';
---
--- # 8
--- # select count(InvoiceId) from invoiceline
---                                      # where InvoiceId=37;
---
--- # 9
--- # select count(InvoiceId) from invoiceline
---                                      # group by InvoiceId
---
---         # 10
--- # select invoiceline.*, Name from invoiceline join track t on t.TrackId = invoiceline.TrackId
-
+# # postgresql
+# # Lab2
+# # 1. Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+# select "CustomerId", "FirstName", "LastName", "Country"
+# from "Customer"
+# where "Country" != 'USA';
+#
+# # 2. Provide a query only showing the Customers from Brazil.
+# select *
+# from "Customer"
+# where "Country" = 'Brazil';
+#
+# # 3. Provide a query showing the Invoices of customers who are from Brazil. The resultant table
+# # should show the customer’s full name, Invoice ID, Date of the invoice and billing country.
+# select "FirstName", "LastName", "InvoiceId", "InvoiceDate", "Country"
+# from "Customer",
+#      "Invoice"
+# where "Country" = 'Brazil';
+#
+# # 4. Provide a query showing only the Employees who are Sales Agents.
+# select *
+# from "Employee"
+# where "Title" like '%Sales%';
+#
+# # 5. Provide a query showing a unique list of billing countries from the Invoice table. ️. Provide a
+# # query that shows the invoices associated with each sales agent. The resultant table should
+# # include the Sales Agent’s full name.
+# select DISTINCT "BillingCountry"
+# from "Invoice";
+# select distinct C."LastName", C."FirstName"
+# from "Employee"
+#          join "Customer" C
+#               on "Employee"."EmployeeId" = C."SupportRepId"
+#          join "Invoice" I
+#               on C."CustomerId" = I."CustomerId"
+# where "Title" like '%Sales%';
+#
+# # 6. Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+# select "Total",
+#        C."FirstName",
+#        C."LastName",
+#        C."Country",
+#        "Title",
+#        E."FirstName",
+#        E."LastName"
+# from "Invoice"
+#          join "Customer" C
+#               on "Invoice"."CustomerId" = C."CustomerId"
+#          join "Employee" E
+#               on C."SupportRepId" = E."EmployeeId"
+# where "Title" ilike ('%sale%');
+# # ilike поиск без учёта регистра в postgresql
+#
+#
+# # Mysql
+# # 7. How many Invoices were there in 2009 and 2011? What are the respective total sales for each of those years?
+# select count(InvoiceDate)
+# from invoice
+# where date(InvoiceDate) >= '2009/01/01'
+#   and date(InvoiceDate) <= '2011/01/01';
+#
+# # 8. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+# select count(InvoiceId)
+# from invoiceline
+# where InvoiceId = 37;
+#
+# # 9. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+# select count(InvoiceId)
+# from invoiceline
+# group by InvoiceId;
+#
+# # 10. Provide a query that includes the track name with each invoice line item.
+# select invoiceline.*, Name
+# from invoiceline
+#          join track t on t.TrackId = invoiceline.TrackId;
+#
--- src/main/resources/lab4.sql
+++ src/main/resources/lab4.sql
@@ -1,10 +1,11 @@
-# lab 4
-# 1
-# select BillingCountry, sum(Total) from invoice
+# # lab 4
+# # 1. Provide a query that shows the total sales per country. Which country’s customers spent the most?
+# select BillingCountry, sum(Total)
+# from invoice
 # group by BillingCountry
 # order by sum(Total) desc;
-
-# 2
+#
+# # 2. Provide a query that shows the most purchased track of 2013.
 # select Name, sum(Total)
 # from invoice
 #          join invoiceline i on invoice.InvoiceId = i.InvoiceId
@@ -12,8 +13,8 @@
 # where date(InvoiceDate) between '2013-01-01' and '2014-01-01'
 # group by Name
 # order by sum(Total) desc;
-
-# 3
+#
+# # 3. Provide a query that shows the top 5 most purchased tracks over all.
 # select Name, sum(Total)
 # from invoice
 #          join invoiceline i on invoice.InvoiceId = i.InvoiceId
@@ -21,18 +22,19 @@
 # group by Name
 # order by sum(Total) desc
 # limit 5;
-
-# # 4
-# select a2.Name, sum(Total) from invoice
-# join invoiceline i on invoice.InvoiceId = i.InvoiceId
-# join track t on t.TrackId = i.TrackId
-# join album a on a.AlbumId = t.AlbumId
-# join artist a2 on a2.ArtistId = a.ArtistId
+#
+# # 4. Provide a query that shows the top 3 best selling artists.
+# select a2.Name, sum(Total)
+# from invoice
+#          join invoiceline i on invoice.InvoiceId = i.InvoiceId
+#          join track t on t.TrackId = i.TrackId
+#          join album a on a.AlbumId = t.AlbumId
+#          join artist a2 on a2.ArtistId = a.ArtistId
 # group by a2.Name
 # order by sum(total) desc
 # limit 3;
-
-# 5
+#
+# # 5. Provide a query that shows the most purchased Media Type.
 # select m.Name, sum(Total)
 # from invoice
 #          join invoiceline i on invoice.InvoiceId = i.InvoiceId
@@ -40,10 +42,11 @@
 #          join mediatype m on m.MediaTypeId = t.MediaTypeId
 # group by m.Name
 # order by sum(total) desc;
-
-# 6
-# select Name, count(GenreId) from invoice
-# join invoiceline i on invoice.InvoiceId = i.InvoiceId
-# join track t on t.TrackId = i.TrackId
+#
+# # 6. Provide a query that shows the number tracks purchased in all invoices that contain more than one genre.
+# select Name, count(GenreId)
+# from invoice
+#          join invoiceline i on invoice.InvoiceId = i.InvoiceId
+#          join track t on t.TrackId = i.TrackId
 # group by Name
-# having count(GenreId)>=2;
+# having count(GenreId) >= 2;
